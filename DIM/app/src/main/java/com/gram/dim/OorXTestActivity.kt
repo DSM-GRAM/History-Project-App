d3ba97e1644fcddb3f5e586f5ef7e7b4f82ea1ea
jinusong <sugwer7464@gmail.com>
2018-11-05 23:39:33 +0900

    [Update] TestLayout

---
+++ DIM/app/src/main/java/com/gram/dim/OorXTestActivity.kt
@@ -0,0 +1,26 @@
+package com.gram.dim
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_o_x.*
+
+
+class OorXTestActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_o_x)
+
+        setSupportActionBar(toolbar_test)
+        arrow_back.setOnClickListener { v ->
+            finish()
+        }
+        btn_o.setOnClickListener { v ->
+            v.visibility = View.GONE
+        }
+        btn_x.setOnClickListener { v ->
+            v.visibility = View.GONE
+        }
+    }
+}
