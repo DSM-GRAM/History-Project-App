c27578ae55f0a953839292c6975c378b78c9e29b
Woopa01 <whdnwls49@naver.com>
2018-11-07 08:32:47 +0900

    add InfoOfPlaceActivity

---
+++ DIM/app/src/main/java/com/gram/dim/InfoOfPlaceActivity.kt
@@ -0,0 +1,13 @@
+package com.gram.dim
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class InfoOfPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_of_place)
+
+
+    }
+}
