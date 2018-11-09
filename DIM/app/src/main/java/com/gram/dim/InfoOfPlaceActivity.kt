95fe43d436f96cd82e03e4be00d884515bda0b9e
Woopa01 <whdnwls49@naver.com>
2018-11-09 12:08:48 +0900

    Add Map

--- DIM/app/src/main/java/com/gram/dim/InfoOfPlaceActivity.kt
+++ DIM/app/src/main/java/com/gram/dim/InfoOfPlaceActivity.kt
@@ -4,19 +4,23 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_info_of_place.*
 
-class InfoOfPlaceActivity : AppCompatActivity(),View.OnClickListener {
+class InfoOfPlaceActivity : AppCompatActivity(),View.OnClickListener,OnMapReadyCallback {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_of_place)
-
         info_of_place_panorama_img_expand.loadImageFromBitmap(BitmapFactory.decodeResource(resources,R.drawable.panorama_example),null)
-
+        (supportFragmentManager.findFragmentById(R.id.info_of_place_map_map_expand) as SupportMapFragment).getMapAsync(this)
         info_of_place_panorama_btn.setOnClickListener(this)
         info_of_place_map_btn.setOnClickListener(this)
 
         info_of_place_back_btn.setOnClickListener { finish() }
+
     }
 
 
@@ -32,5 +36,9 @@ class InfoOfPlaceActivity : AppCompatActivity(),View.OnClickListener {
             info_of_place_map_btn.setBackgroundResource(R.drawable.back_btn_expand_round_shape_placeinfo)
             info_of_place_map_map.expand()
         }
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+
     }
 }
