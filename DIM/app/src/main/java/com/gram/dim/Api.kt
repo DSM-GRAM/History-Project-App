1dbe7c4b79e52447b9dc0d5b076e61a8af39eecc
Jeongdasilver <alal8272@gmail.com>
2019-01-17 12:30:15 +0900

    UPDATE Api

--- DIM/app/src/main/java/com/gram/dim/Api.kt
+++ DIM/app/src/main/java/com/gram/dim/Api.kt
@@ -1,5 +1,6 @@
 package com.gram.dim
 
+import com.gram.dim.Model.HistoricalSiteInformModel
 import com.gram.dim.Model.SelectHistoricalSiteModel
 import retrofit2.Call
 import retrofit2.http.GET
@@ -11,4 +12,7 @@ interface Api {
     @Headers("Content-Type: application/json")
     fun getSites(@Path("area") area: String): Call<ArrayList<SelectHistoricalSiteModel>>
 
+    @GET("main/{area}/{history_site_name}")
+    @Headers("Content-Type: application/json")
+    fun getSiteInform(@Path("area") area: String, @Path("history_site_name") historySiteName: String): Call<HistoricalSiteInformModel>
 }
